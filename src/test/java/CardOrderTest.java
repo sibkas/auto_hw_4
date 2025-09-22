284c13dad1cb5001f95ab6077e12077ca2553342
sibkas <106993347+sibkas@users.noreply.github.com>
2025-09-22 16:04:54 +0700

    Update CardOrderTest.java

--- src/test/java/CardOrderTest.java
+++ src/test/java/CardOrderTest.java
@@ -19,7 +19,7 @@ public class CardOrderTest {
     void setup() {
         Configuration.headless = true;
         Configuration.browser = "chrome";
-        Configuration.driverManagerEnabled = false; 
+         
         Configuration.browserBinary = "/opt/hostedtoolcache/setup-chrome/chrome/117.0.5938.149/x64/chrome";
     
         ChromeOptions options = new ChromeOptions();
